61ec8279b011996cf64b7343c39c99fff3065b18
Stevo5o <stevo.joc@gmail.com>
2015-03-07 13:16:13 +0000

    Basic function addition

---
+++ public_html/script.js
@@ -0,0 +1,21 @@
+/**
+ * script.js 
+ * @ Stephen O'Connor, March 2015
+ * 
+ * Dependencies:
+ * 
+ */
+
+// immediately invoked anonymous function
+(function () {
+
+    plus = function (a, b) {
+        var sum = a + b;
+        return sum;
+    }
+
+    console.log(plus(2, 2));
+
+}()); // end anon fun
+
+
