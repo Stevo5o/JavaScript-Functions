861ebb26bcc62a3a6542c67223649551c6542957
Stevo5o <stevo.joc@gmail.com>
2015-03-10 09:59:37 +0000

    Getting to grips with 'this'

--- public_html/script.js
+++ public_html/script.js
@@ -8,15 +8,20 @@
 
 // immediately invoked anonymous function
 (function() {
-
-	plus = function(a, b) {
-		return (
-			console.log(a + b),
+// The 'this' arg points to the object 
+// Invoke 'this' using dot notation 
+// Binding 'this' happens at invocation time
+ var calc = {
+ 	status: 'Nice',
+	plus: function(a, b) {
+		return (			
 			console.log(this),
-			console.log(arguments)
+			console.log(a + b),
+			console.log(arguments),
+			console.log(this.status)
 		);
-	};
-
-	plus(2, 2);
+	}
+};
+	calc.plus(2, 2);
 
 }()); // end anon func
