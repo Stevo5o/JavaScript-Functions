a7f3d2372b29972332ac355b395103d16988c826
Stevo5o <stevo.joc@gmail.com>
2015-03-10 10:50:17 +0000

    prototype - object

--- public_html/script.js
+++ public_html/script.js
@@ -8,23 +8,34 @@
 
 // immediately invoked anonymous function
 (function() {
-	// new creates a new instance of the obj
-	// Each new instance has it's own set of properties
-	// 'this' arg points to the instance of the obj
-	// Constructor names are capitalized 
+	// Prototypal Inheritance 
+	// Every object can be based
+	// Multiple objs can inherit 
+	// All objs inherit properties
+	// Declarations inherit from Function
+	// Function constructor inherits from Object
+	var speak = function(saywhat) {
+		console.log(saywhat);
+	};
+
 	var Dog = function() {
 		var name, breed;
-		return console.dir(this);
-	}
+	};
+
+	var Cat = function() {
+		var name, breed;
+	};
+
+	Dog.prototype.speak = speak;
+	Cat.prototype.speak = speak;
 
 	firstDog = new Dog;
-	firstDog.name = 'Rover';
-	firstDog.breed = 'Doberman';
+	firstDog.name = "Rover";
+	firstDog.breed = "Doberman";
+	firstDog.speak('woof');
 
-	secondDog = new Dog;
-	secondDog.name = 'Fluffy';
-	secondDog.breed = 'Poodle';
-
-	console.log(firstDog.name);
-	console.log(secondDog.name);
+	firstCat = new Cat;
+	firstCat.name = "Tiger";
+	firstCat.breed = "Bengal";
+	firstCat.speak('meow');
 }()); // end anon func
